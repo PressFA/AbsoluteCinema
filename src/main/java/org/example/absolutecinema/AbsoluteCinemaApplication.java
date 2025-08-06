42d145f2c4097158ea1309aedb94688dd443bc71
PressF <artemsurmenok@gmail.com>
2025-08-06 17:02:46 +0300

    Remove 2 empty lines in the main config class

--- src/main/java/org/example/absolutecinema/AbsoluteCinemaApplication.java
+++ src/main/java/org/example/absolutecinema/AbsoluteCinemaApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AbsoluteCinemaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AbsoluteCinemaApplication.class, args);
 	}
-
 }
