196040d8124ad2a2fc60cf342f90793e18179cf7
PressF <artemsurmenok@gmail.com>
2025-08-06 16:22:37 +0300

    Initial commit

---
+++ src/test/java/org/example/absolutecinema/AbsoluteCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.absolutecinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsoluteCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
